73340f39d5bcf644ba526417dbd83265775867a3
drNBrio <andy.girvy@gmail.com>
2021-09-16 22:34:53 +1200

    Update Program.vb

--- Program.vb
+++ Program.vb
@@ -4,5 +4,6 @@ Module Program
     Sub Main(args As String())
         Console.WriteLine("Hello World!")
         Console.WriteLine("Goodbye World!")
+        Console.WriteLine("Goodnight World!")
     End Sub
 End Module
